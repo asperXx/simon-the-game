9af1e1375217882790a00de3e3c200206fccaa3b
AsperXx <varfol0077@gmail.com>
2020-08-12 17:41:42 +0500

    add animate, logic, levels

--- src/App.vue
+++ src/App.vue
@@ -1,22 +1,151 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <div class="simon">
+      <h1>{{title}}</h1>
+      <div class="simon__row">
+        <div class="simon__buttons">
+          <button :class="{light: light[1]}" :disabled="!userTurn" class="btnGreen" @click="clickedBtn(1)"></button>
+          <button :class="{light: light[2]}" :disabled="!userTurn" class="btnYellow" @click="clickedBtn(2)"></button>
+          <button :class="{light: light[3]}" :disabled="!userTurn" class="btnRed" @click="clickedBtn(3)"></button>
+          <button :class="{light: light[4]}" :disabled="!userTurn" class="btnBlue" @click="clickedBtn(4)"></button>
+        </div>
+        <div class="simon__menu">
+          <h2 class="round">Round: {{round}}</h2>
+
+          <button @click="start">{{startBtn}}</button>
+          <h2>Level:</h2>
+          <ul>
+            <li v-for="(lvl,index) in levels" :key="index">
+              <input name="level" :value="index" :id="index" type="radio" v-model="lvlPicked" />
+              <label :for="lvl">{{index}}</label>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+var timer;
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+  data() {
+    return {
+      round: 0,
+      startBtn: "Start",
+      title: 'Simon The Game',
+      userTurn: false,
+      light: {
+        1: false,
+        2: false,
+        3: false,
+        4: false,
+      },
+      index: 0,
+      sequence: [],
+      lvlPicked: "Eazy",
+      levels: {
+        Eazy: 1500,
+        Medium: 1000,
+        Hard: 500,
+      },
+    };
+  },
+  computed:{
+    delay() {
+      if (this.lvlPicked === "Eazy") {
+        return this.levels.Eazy
+      } else if (this.lvlPicked === "Medium") {
+        return this.levels.Medium
+      } else { return this.levels.Hard}
+    }
+  },
+  methods: {
+    start() {
+      if (this.startBtn === "Start") {
+        this.startBtn = 'Stop';
+        this.simonTurn();
+      } else {
+        this.reset();
+      }
+    },
+    simonTurn() {
+      this.index = 0;
+      this.round++;
+      this.sequence.push(this.randomNumber());
+      this.showSequence(() => {
+        this.userTurn = true;
+      })
+    },
+    showSequence(callback) {
+      var self = this;
+      var i = 0;
+      timer = setInterval(function () {
+        if(i>=self.sequence.length) {
+          self.stopInterval();
+        }
+        self.animate(self.sequence[i]);
+        i++;
+      }, this.delay);
+      callback();
+    },
+    stopInterval: function(){
+      clearInterval(timer);
+    },
+    randomNumber() {
+      return Math.floor(Math.random() * 4 + 1);
+    },
+    reset() {
+      this.startBtn = "Start";
+      this.sequence = [];
+      this.lvlPicked = "Eazy";
+      this.userTurn = false;
+      this.round = 0;
+      this.index = 0;
+    },
+    clickedBtn(num) {
+      this.animate(num);
+      var isCorrect = this.checkSequence(num);
+      if (!isCorrect) {
+        this.gameOver();
+        return;
+      } else {
+        if (this.index === this.sequence.length - 1) {
+          setTimeout(() => {
+            this.userTurn = false;
+            this.simonTurn();
+          }, this.delay);
+        } else {
+        this.index++;
+      }
+      } 
+    },
+    checkSequence(num) {
+      return (this.sequence[this.index] === num);
+    },
+    animate(num) {
+      this.light[num] = true;
+      var audio = new Audio(require('./assets/sounds/' + num + '.mp3'))
+      audio.play();
+      // Можно использовать self для переопределения this, но и так читаемость нормальная
+      setTimeout(() => {
+            this.light[num] = false;
+          }, this.delay);
+          return;
+    },
+    gameOver() {
+      this.title = 'Game over!'
+      this.reset();
+    }
+  },
+};
 </script>
 
-<style>
+<style lang="scss">
+* {
+  margin: 0;
+  padding: 0;
+}
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
@@ -24,5 +153,74 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+  display: flex;
+  justify-content: center;
+}
+
+.simon {
+  &__row {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    padding: 50px;
+    .simon__buttons {
+      display: flex;
+      flex-wrap: wrap;
+      align-items: center;
+      justify-content: center;
+      width: 400px;
+      button {
+        height: 150px;
+        width: 150px;
+        border: none;
+        outline: none;
+        opacity: 0.6;
+      }
+    }
+  }
+  &__menu {
+    display: flex;
+    flex-direction: column;
+    padding: 30px;
+    h2 {
+      margin-bottom: 10px;
+    }
+    button {
+      margin-bottom: 20px;
+    }
+    ul {
+      display: flex;
+      flex-direction: column;
+      align-items: flex-start;
+      justify-content: flex-start;
+      list-style: none;
+      padding: 3px;
+      li {
+        padding: 2px;
+        label {
+          margin-left: 10px;
+        }
+      }
+    }
+  }
+}
+.light {
+  opacity: 1 !important;
+}
+.btnGreen {
+  background-color: dodgerblue;
+  border-top-left-radius: 100%;
+}
+.btnYellow {
+  background-color: #ff5643;
+  border-top-right-radius: 100%;
+}
+.btnRed {
+  background-color: #feef33;
+  border-bottom-left-radius: 100%;
+}
+.btnBlue {
+  background-color: #bede15;
+  border-bottom-right-radius: 100%;
 }
 </style>
--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,58 +1,217 @@
 <template>
   <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
+   <div id="app">
+  
+</div>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'HelloWorld',
-  props: {
-    msg: String
-  }
+ 
 }
 </script>
 
-<!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
-h3 {
-  margin: 40px 0 0;
+<style lang="scss">
+$color-black: #000;
+$color-dark-gray: #2e2e2e;
+$color-med-gray: #666;
+$color-light-gray: #ccc;
+$color-green: darken(green, 5%);
+$color-red: darken(red, 10%);
+$color-yellow: darken(#ffd700, 10%);
+$color-blue: darken(#0000cd, 5%);
+$color-white: #fff;
+$color-light-green: lighten(green, 5%);
+$color-light-red: lighten(red, 5%);
+$color-light-blue: lighten(#0000cd, 5%);
+$color-light-yellow: lighten(#ffd700, 5%);
+$color-bisque: #ffe4c4;
+
+// Main Styles
+body {
+  font-family: 'Open Sans', sans-serif;
+  user-select: none;
+  background-color: $color-bisque;
 }
-ul {
-  list-style-type: none;
-  padding: 0;
+h1 {
+  font-family: 'Russo One', sans-serif;
+  font-size: 3rem;
+  cursor: default;
+  margin: 1rem 0;
 }
-li {
+#simon {
+  margin: 2rem auto 0 auto;
+  max-width: 680px;
+  min-width: 680px;
+  max-height: 680px;
+  min-height: 680px;
+  background-color: $color-dark-gray;
+  border-radius: 50%;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  position: relative;
+  box-shadow: 0px 0px 18px rgba(0,0,0,.6)
+}
+#buttons {
+  display: flex;
+  flex-wrap: wrap;
+  max-width: 620px;
+  min-width: 620px;
+  justify-content: space-between;
+}
+.button {
+  cursor: pointer;
+  min-width: 300px;
+  min-height: 300px;
+  max-width: 300px;
+  max-height: 300px;
+}
+#green { 
+  background-color: $color-green; 
+  border-top-left-radius: 300px;
+  margin-bottom: 20px;
+  box-shadow: inset 3px 3px 10px rgba(255,255,255,.3);
+}
+#blue { 
+  background-color: $color-blue; 
+  border-bottom-right-radius: 300px;
+  box-shadow: inset -3px -3px 10px rgba(255,255,255,.3);
+}
+#yellow { 
+  background-color: $color-yellow; 
+  border-bottom-left-radius: 300px;
+  box-shadow: inset 3px -3px 10px rgba(255,255,255,.3);
+}
+#red { 
+  background-color: $color-red; 
+  border-top-right-radius: 300px;
+  margin-bottom: 20px;
+  box-shadow: inset -3px 3px 10px rgba(255,255,255,.3);
+}
+#green.highlight { background-color: $color-light-green; }
+#red.highlight { background-color: $color-light-red; }
+#blue.highlight { background-color: $color-light-blue; }
+#yellow.highlight { background-color: $color-light-yellow; }
+
+// Center Controls
+#center {
+  border: 20px solid $color-dark-gray;
+  box-sizing: border-box;
+  position: absolute;
+  min-width: 300px;
+  max-width: 300px;
+  min-height: 300px;
+  max-height: 300px;
+  border-radius: 50%;
+  background-color: $color-light-gray;
+  display: flex;
+  flex-direction: column;
+  justify-content: flex-start;
+  align-items: center;
+  color: $color-dark-gray;
+}
+#title {
+  margin-top: 20px;
+  text-align: center;
+  background-color: $color-light-gray;
+  width: 200px;
+  border-top-left-radius: 50%;
+  border-top-right-radius: 50%;
+}
+#controls {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  min-width: 180px;
+  margin-top: .5rem;
+}
+#counter {
   display: inline-block;
-  margin: 0 10px;
+  width: 60px;
+  height: 40px;
+  color: $color-med-gray;
+  background-color: $color-black;
+  border: 4px solid $color-dark-gray;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  border-radius: 8px;
+  font-size: 1.6rem;
 }
-a {
-  color: #42b983;
+.lit { color: $color-red; }
+#power {
+  margin-top: 1rem;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  width: 110px;
+  font-size: .875rem;
+  text-transform: uppercase;
+  font-weight: bold;
+}
+#switch { 
+  cursor: pointer;
+  display: inline-block; 
+  width: 50px;
+  height: 25px;
+  background-color: $color-dark-gray;
+  position: relative;
+}
+#switch:after {
+  content: '';
+  position: absolute;
+  top: 0;
+  height: 19px;
+  width: 19px;
+  border: 3px solid $color-dark-gray; 
+  background-color: $color-red;
+}
+#switch.off:after { left: 0 }
+#switch.on:after { right: 0 }
+.btn {
+  cursor: pointer;
+  position: relative;
+  display: inline-block;
+  width: 24px;
+  height: 24px;
+  background-color: $color-yellow;
+  border-radius: 50%;
+  border: 4px solid $color-dark-gray;
+  margin-top: 1rem;
+  box-shadow: 2px 2px 3px rgba(0,0,0,.4);
+}
+.btn::after {
+  position: absolute;
+  top: -20px;
+  left: -25%;
+  font-size: 12px;
+  text-transform: uppercase;
+  font-weight: bold;
+}
+.btn:hover { background-color: $color-light-yellow; }
+#start.btn { background-color: $color-red; }
+#start.btn:hover { background-color: $color-light-red; }
+#strict.btn::before {
+  position: absolute;
+  content: '';
+  top: -34px;
+  left: 7px;
+  display: inline-block;
+  width: 6px;
+  height: 6px;
+  border: 2px solid $color-dark-gray;
+  border-radius: 50%;
+  background-color: $color-black;
+}
+#strict.strict.btn::before { background-color: $color-yellow; }
+#start::after { content: 'Start'; }
+#strict::after { content: 'Strict'; }
+
+footer {
+  padding: 3rem 0;
+  text-align: center;
+  font-size: .875rem;
 }
 </style>
